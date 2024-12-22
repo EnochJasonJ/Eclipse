0a21e7add909407577979497421351fa72b528de
Enoch Jason J <enochjason06@gmail.com>
2024-12-22 12:24:54 +0530

    Vue js updated

---
+++ front-end/e-commerce/src/components/NavBar.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import {RouterLink} from 'vue-router'
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li> <RouterLink to="/home">Home</RouterLink> </li>
+        </ul>
+    </nav>
+</template>
